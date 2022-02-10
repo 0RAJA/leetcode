69feac3eafc775719a0b9945a2261beb4638d43a
raja <1647193241@qq.com>
2022-02-10 23:01:05 +0800

    测试

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hjh")
+	testMap()
+}
+
+func testMap() {
+	type Num struct {
+		a, b int
+	}
+	m := make(map[Num]bool)
+	fmt.Println(m[Num{a: 1, b: 2}])
+	m[Num{a: 1, b: 2}] = true
+	fmt.Println(m[Num{a: 1, b: 2}])
 }
