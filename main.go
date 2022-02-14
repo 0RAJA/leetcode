ced9d1e85b83f3fa68a5c019b5c70f30e36829e3
raja <1647193241@qq.com>
2022-02-14 21:15:14 +0800

    测试

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
+/*
+ * @Author: Raja
+ * @Description:
+ * @Date: 2022-02-12 07:17:21
+ * @LastEditTime: 2022-02-14 04:34:06
+ * @FilePath: /leetcode/main.go
+ */
 package main
 
 import "fmt"
 
 func main() {
 	testMap()
+	testMMM()
 }
 
 func testMap() {
@@ -14,4 +22,8 @@ func testMap() {
 	fmt.Println(m[Num{a: 1, b: 2}])
 	m[Num{a: 1, b: 2}] = true
 	fmt.Println(m[Num{a: 1, b: 2}])
+}
+
+func testMMM() {
+	fmt.Println("testMaa")
 }
